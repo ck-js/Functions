68e80a324e325fe8dc2f16a7b36082e95757cf02
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-15 14:28:05 +0700

    Add functions with multple parameters and stored and called them in a variable, used the return statement to call the variable with our function parameters

--- script.js
+++ script.js
@@ -27,6 +27,39 @@ function displayHalf(number) {
 }
 displayHalf(1000);
 
+function greaterThan10(number) {
+const result = number > 10;
+return result;
+}
+console.log(greaterThan10(20));
+
+function add20(number) {
+    const addTwenty = 20 + number;
+    return addTwenty;
+}
+const result = add20(50);
+console.log(result);
+
+function topThree(first,second,third) {
+    return first + second + third;
+}
+let result2 = topThree("Drake", "Post Malone", "Russ");
+console.log(result2);
+
+function topViral(first,second,third) {
+    const viral = "1st: " + first + " Second: " + second + "Third: " + third
+return viral;
+}
+const result3 = topViral("Bad Bunny", "Shakira", "Drake");
+console.log(result3);
+
+
+
+
+
+
+
+
 
 
 

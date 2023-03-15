c3b529a4f96c563a7580bb802d6d1693efefa23b
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-15 12:32:00 +0700

    Add functions with variables inside of the code block that displays the variables value. Add function using parameters and store in a variable then display in a console log. Practice calling functions with different parameters

---
+++ script.js
@@ -0,0 +1,34 @@
+function greetUser() {
+    console.log("Hello User");
+}
+greetUser();
+
+function lampStatus() {
+    const powerOn = true;
+    console.log("Power on: " + powerOn);
+}
+lampStatus();
+
+function activeStatus() {
+    const active = "active";
+    const inActive = "inactive";
+    console.log("Bob is " + inActive);
+}
+activeStatus();
+
+function userStatus(status) {
+    console.log("Bob is " + status);
+}
+userStatus("active")
+
+function displayHalf(number) {
+    const half = number / 2;
+    console.log(half + " is half of " + number);
+}
+displayHalf(1000);
+
+
+
+
+
+
